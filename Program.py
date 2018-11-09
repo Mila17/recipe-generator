d64ebeb9abd2511fdfc62745dda806141be77e96
Mila17 <32477373+Mila17@users.noreply.github.com>
2018-11-09 09:25:28 -0500

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,17 @@
+# Raquel Dombroskie & Milica Djuric Case Study Due Dec.1 Basic Recipe Calculator
+# Raquel doesn't work Mon & Wed
+# Purpose of program:
+
+# 3 files
+
+# Read data from 1st file (maybe turn into list or dict)
+# Loop through 2nd file
+# Compare both files for a process
+# Save to a 3rd file.
+
+#Ex.
+# 1st file opened and saved to list. (ingredients of fridge&pantry)
+# 2nd file contains (Recipes matched with ingredients)
+# 'PB&J', ['jelly', 'pb', 'bread']
+# 3rd file: generatable recipes availbale with ingredients
+
