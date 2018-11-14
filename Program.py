5058d6c203d3b9298ccbf0c34e33364b978c3cb0
Mila17 <32477373+Mila17@users.noreply.github.com>
2018-11-14 16:07:02 -0500

    Add files via upload

--- Program.py
+++ Program.py
@@ -1,17 +1,65 @@
-# Raquel Dombroskie & Milica Djuric Case Study Due Dec.1 Basic Recipe Calculator
-# Raquel doesn't work Mon & Wed
-# Purpose of program:
+# Cookbook (Case Study) by Raquel Dombroskie & Milica Djuric Case Due Dec.1
 
-# 3 files
+# Purpose of program: Compare ingredients
+# and match with available recipes that can be made.
 
-# Read data from 1st file (maybe turn into list or dict)
-# Loop through 2nd file
-# Compare both files for a process
-# Save to a 3rd file.
+# Takes ingredients input file.
 
-#Ex.
-# 1st file opened and saved to list. (ingredients of fridge&pantry)
-# 2nd file contains (Recipes matched with ingredients)
-# 'PB&J', ['jelly', 'pb', 'bread']
-# 3rd file: generatable recipes availbale with ingredients
+ingredients = []
+recipes = {}
 
+def main():
+    ingredients_file = open('ingredients.txt', 'r')
+    for line in ingredients_file:
+        line = line.rstrip('\n')
+        ingredients.append(line)
+
+    selection = 0
+    while selection != '6':
+        selection = options()
+        if selection == '1':
+            print("This is what you have:", ingredients)
+        elif selection == '2':
+            #append to the list and at bottom of program save to ingredients.txt
+            ingredient = input("Add ingredient: ")
+            ingredients.append(ingredient)
+        elif selection == '3':
+            to_delete = input("Ingredient to delete: ")
+            ingredients.remove(to_delete)
+        elif selection == '4':
+            recipe_name = input("Recipe name: ")
+            recipe_ingredients = input("Enter each ingredient separated by a space: ")
+            recipe_list = []
+            recipe_list = recipe_ingredients.split()
+            recipes[recipe_name] = recipe_list
+        elif selection == '5':
+            print(recipes)
+            
+            
+    
+    ingredients_file = open('ingredients.txt', 'w')
+    for item in ingredients:
+        ingredients_file.write(item + '\n')
+    
+    #save dictionary to file with pickel and load it back in
+
+
+
+
+
+
+    
+def options():
+    print("\nOptions")
+    print("1) Show ingredients")
+    print("2) Add ingredients")
+    print("3) Delete ingredients")
+    print("4) Add a recipe")
+    print("5) See recipes")
+    print("6) Quit")
+    selection = input("Your choice: ")
+    return selection
+
+
+
+main()
